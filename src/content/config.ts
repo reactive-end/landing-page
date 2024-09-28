172300d7299eb53d12130628fbc1a3ceb9191bbc
reactive-end <rafaelpisani19@gmail.com>
2024-09-28 11:33:25 -0400

    First Commit

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content'
+
+const people = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    image: z.string(),
+    phone: z.string()
+  }),
+  type: 'data'
+})
+
+export const collections = { people }
