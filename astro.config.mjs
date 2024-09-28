b06b8b28cff51b750e3b7ccda08ee3d36c3e4ca9
reactive-end <rafaelpisani19@gmail.com>
2024-09-28 11:42:10 -0400

    Add Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import solidJs from '@astrojs/solid-js';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel()
 });
