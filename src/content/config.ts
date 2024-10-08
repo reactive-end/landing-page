04921fb09990ae5d3e1f76ff1793c19ca20a9d8e
reactive-end <rafaelpisani19@gmail.com>
2024-10-07 22:11:20 -0400

    Added New Styles

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,8 @@ const people = defineCollection({
   schema: z.object({
     name: z.string(),
     description: z.string(),
+    subDescription: z.string(),
+    slogan: z.string(),
     image: z.string(),
     phone: z.string()
   }),
